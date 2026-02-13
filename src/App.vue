c9cc3a15102a6f7f9d8728b491e48155d7ccdd91
Luis <morilloalburguelf@uvm.edu.ve>
2026-02-12 23:45:59 -0400

    Seccion con barra de busqueda agregada

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,69 @@
 <template>
   <body>
     <HeaderComp></HeaderComp>
-    <h1>Proyecto</h1>
+    <div class="searchbox-bar">
+      <h2 class="title">Conecta con tu Ciudad</h2>
+      <p>Descubre emprendimientos verificados cerca de ti</p>
+      <form onsubmit="#;" role="search">
+        <input type="serach" class="search-input" id="search-input" name="search-input" placeholder="Buscar...">
+        <button class="button" type="button">
+          <span>Buscar</span>
+        </button>
+      </form>
+    </div>
     <FooterComp></FooterComp>
   </body>
 </template>
 
 <style scoped>
   body{
-    padding-top: 10%;
+    padding-top: 6.5rem;
     padding-bottom: 10%;
+  }
+
+  .searchbox-bar{
+    background-color: lightgray;
+    width: 100%;
+    min-height: 250px;
+    right: 0;
+    position: absolute;
+    align-content: center;
+    text-align: center;
+  }
+
+  .title{
+    font-weight: lighter;
+    font-size: 2.3rem;
+  }
+
+  .search-input {
+  margin: 20px;
+  outline: none;
+  }
+
+  
+
+  .button{
+    --button-background: #fff;
+    --button-background-hover: #eee;
+    --button-color: #333;
+
+    display: inline-flex;
+    align-items: center;
+    gap: 6px;
+    background: var(--button-background);
+    color: var(--button-color);
+    outline: none;
+    border: solid;
+    border-radius: 5px;
+    border-width: 2px;
+    border-color: #1f2e3e;
+    padding: 6px 12px;
+    cursor: pointer;
+  }
+
+  .button:hover,
+  .button:active{
+    background-color: var(--button-background-hover);
   }
 </style>
